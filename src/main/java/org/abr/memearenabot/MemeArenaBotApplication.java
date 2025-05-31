71c6ce4556357a71612b466bb0fceb9e883d696d
DenisAbramenko <den-a96@mail.ru>
2025-05-31 14:56:04 +0300

    first commit

---
+++ src/main/java/org/abr/memearenabot/MemeArenaBotApplication.java
@@ -0,0 +1,13 @@
+package org.abr.memearenabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeArenaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeArenaBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/abr/memearenabot/MemeArenaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abr.memearenabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeArenaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

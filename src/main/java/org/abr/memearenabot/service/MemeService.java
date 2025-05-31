9bb0be89aff87cb385009847be75e6398f377ffa
DenisAbramenko <den-a96@mail.ru>
2025-05-31 15:34:44 +0300

    add MemeService.java

--- src/main/java/org/abr/memearenabot/service/MemeService.java
+++ src/main/java/org/abr/memearenabot/service/MemeService.java
@@ -1,4 +1,12 @@
 package org.abr.memearenabot.service;
 
 public class MemeService {
+    public MemeService() {
+    }
+
+    public String generateMeme(String description) {
+        // Пример вызова внешнего API, например, DALL·E или HuggingFace
+        // Здесь будет логика генерации картинки + добавление текста
+        return "https://meme-storage.com/memes/generated_meme.jpg";
+    }
 }
